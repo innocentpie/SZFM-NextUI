f01ec10d95f63e83d6e5a27bf0d1725dd5340076
Szabados Gergő <gergo.szabados11@gmail.com>
2024-12-06 17:55:47 +0100

    ui: quiz text size

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -233,7 +233,7 @@ export default function QuizPage({ params }: { params: {id: string }} ){
       <div className='center-col'>
           <Card className='main-card'>
             <CardHeader className='justify-center'>
-              <p className='text-xl font-bold text-center'>Gratulálunk!</p>
+              <p className='text-3xl font-bold text-center'>Gratulálunk!</p>
             </CardHeader>
             <CardBody>
               <div className='card-body'>
